6ec274a103902e3e1e05a885a23da13db3c05653
Max Sierra <cuentas_iMax@live.com>
2015-03-02 11:49:10 -0600

    Versión 1

---
+++ index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>deCIR UADY</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/justified-nav.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style_list.css" />
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+    <script src="js/modernizr.custom.63321.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if gte IE 8]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!--[if lt IE 8]>
+      <link href="ie7.css" rel="stylesheet" type="text/css" />
+    <![endif]-->
+
+    <?php
+  function dameURL(){
+    $url=$_SERVER['REQUEST_URI'];
+    //echo strlen($url)."- ";
+    return $url;
+  } 
+?>
+
+  </head>
+
+  <body>
+
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar" >
+                <span class="sr-only" >Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav" >
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Nav header</li>
+                    <li><a href="#">Separated link</a></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="masthead">
+        <!-- <h3 class="text-muted">deCIR</h3> -->
+        <img src="img/header.png" width="100%">
+        <div role="navigation">
+          <ul class="nav nav-justified">
+            <!--<li class="active"><a href="#">Quienes somos</a></li> -->
+            <li><a href="#">Quienes somos</a></li>
+            <li><a href="#">Niños</a></li>
+            <li><a href="#">Juegos</a></li>
+            <li><a href="#">Informacion</a></li>
+            <li><a href="#">Vídeos</a></li>
+            <li><a href="#">Laboratorios</a></li>
+            <!--<li><a href="#">Eventos1</a></li>-->
+            <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Eventos <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Verano</a></li>
+                    <li><a href="#">Congreso de investigación</a></li>
+                    <li><a href="#">Jornadas</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Nav header</li>
+                    <li><a href="#">Separated link</a></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- Jumbotron --> <!--
+      <div class="jumbotron">
+        <h1>Marketing stuff!</h1>
+        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
+      </div> -->
+      <div></div>
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-lg-2" id="lateral">
+          <!--<h2>Safari bug warning!</h2>-->
+          <!--<p class="text-danger">As of v8.0, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>-->
+          <!--<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>-->
+          <!--<p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>-->
+          <section class="main clearfix">
+      
+        <div class="fleft">
+          <select id="cd-dropdown" class="cd-select">
+            <option value="-1" selected>Choose a network to add</option>
+            <option value="1" class="icon-google-plus">Google Plus</option>
+            <option value="2" class="icon-facebook">Facebook</option>
+            <option value="3" class="icon-twitter">Twitter</option>
+            <option value="4" class="icon-github">GitHub</option>
+          </select>
+        </div>
+      </section>
+        </div>
+        <div class="col-lg-8">
+        	 <div class="jumbotron" id="caja">
+	          	<!--<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>-->
+              <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img src="img/verano2012_a.jpg" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Evento 1</h1>
+              <!--<p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>-->
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="img/verano2013_a.jpg" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Evento 2</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
+        	 </div>
+        	<div class="col-lg-8" id="c1">
+        		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        	</div>
+        	<div class="col-lg-4" id="c2">
+
+        		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        	</div>
+       </div>
+        <div class="col-lg-2">
+          <!-- <h2></h2>-->
+          <br/><br/><br/>
+          <p><img src="img/boton_lateral.png" width="100%"></p><br/><br/>
+          <p><img src="img/boton_lateral.png" width="100%"></p><br/><br/>
+          <p><img src="img/boton_lateral.png" width="100%"></p><br/><br/>
+          <p><img src="img/boton_lateral.png" width="100%"></p><br/><br/>
+          <p><img src="img/boton_lateral.png" width="100%"></p><br/><br/>
+        </div>
+      </div>
+
+      <!-- Site footer -->
+      <footer class="footer" id="texto">
+        <p>&copy; UADY 2015. Derechos reservados.</p>
+        <p>&copy; Esta página puede ser reproducida con fines no lucrativos, siempre y cuando no se mutile, se cite la fuente completa y su dirección electrónica, de otra forma requiere permiso previo por escrito de la institución.</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- listas -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.dropdown.js"></script>
+    <script type="text/javascript">
+      
+      $( function() {
+        
+        $( '#cd-dropdown' ).dropdown( {
+          gutter : 5,
+          delay : 100,
+          random : true
+        } );
+
+      });
+
+    </script>
+
+  </body>
+</html>
